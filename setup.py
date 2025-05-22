12e633c8a212705e878e654e58f5ea8ee60a9424
Hadrien <hadrien.bossard.ext@mail.numerique.gouv.fr>
2025-05-22 17:13:57 +0200

    build: add more metadata to pypy

--- setup.py
+++ setup.py
@@ -1,8 +1,12 @@
-#! /usr/bin/env python3
+from pathlib import Path
 
 from setuptools import find_packages, setup
 
-with open('datagouv/__init__.py') as f:
+
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+
+with open(this_directory / "datagouv/__init__.py") as f:
     lines = f.readlines()
     for line in lines:
         if line.startswith("__version__"):
@@ -22,25 +26,34 @@ setup(
     author_email="opendatateam@data.gouv.fr",
     classifiers=[
         "Development Status :: 2 - Pre-Alpha",
-        "License :: OSI Approved :: GNU Affero General Public License v3",
-        "Operating System :: POSIX",
+        "License :: OSI Approved :: MIT",
+        "Operating System :: OS Independent",
         "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
+        "Programming Language :: Python :: 3.13",
+        "Programming Language :: Python :: Implementation :: CPython",
         "Topic :: Scientific/Engineering :: Information Analysis",
     ],
     description="Wrapper for the data.gouv.fr API",
+    long_description=long_description,
     long_description_content_type="text/markdown",
     keywords="api wrapper datagouv",
     license="https://spdx.org/licenses/MIT.html#licenseText",
-    url="https://github.com/datagouv/datagouv_client",
+    url="https://www.data.gouv.fr",
+    project_urls={
+        "Documentation": "https://www.data.gouv.fr/fr/dataservices/api-catalogue-des-donnees-ouvertes-data-gouv-fr/",
+        "Source": "https://github.com/datagouv/datagouv_client",
+    },
     data_files=[
-        ("share/datagouv_client", ["CHANGELOG.md", "LICENSE.AGPL.txt", "README.md"]),
+        ("share/datagouv_client", ["CHANGELOG.md", "LICENSE", "README.md"]),
     ],
     entry_points={
         "console_scripts": [
             "datagouv_client=datagouv_client.cli:run",
         ],
     },
-    include_package_data=True,  # Will read MANIFEST.in
     setup_requires=pip("requirements-build.txt"),
     install_requires=pip("requirements.txt"),
     extras_require={
