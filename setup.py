9a8e2c2ed24052826c4f8fa2707f30e2c9b8fb81
Hadrien Bossard <hadbossard@gmail.Com>
2025-06-06 11:57:26 +0200

    Separate dev dependencies (#9)

--- setup.py
+++ setup.py
@@ -1,6 +1,17 @@
-#! /usr/bin/env python3
+from pathlib import Path
 
 from setuptools import find_packages, setup
+
+
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+
+with open(this_directory / "datagouv/__init__.py") as f:
+    lines = f.readlines()
+    for line in lines:
+        if line.startswith("__version__"):
+            _, _, version = line.replace("'", "").replace('"', "").split()
+            break
 
 
 def pip(filename):
@@ -10,30 +21,43 @@ def pip(filename):
 
 setup(
     name="datagouv_client",
-    version=__import__("datagouv").__version__,
+    version=version,
     author="Etalab",
     author_email="opendatateam@data.gouv.fr",
     classifiers=[
         "Development Status :: 2 - Pre-Alpha",
-        "License :: OSI Approved :: GNU Affero General Public License v3",
-        "Operating System :: POSIX",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
         "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
+        "Programming Language :: Python :: 3.13",
+        "Programming Language :: Python :: Implementation :: CPython",
         "Topic :: Scientific/Engineering :: Information Analysis",
     ],
     description="Wrapper for the data.gouv.fr API",
+    long_description=long_description,
     long_description_content_type="text/markdown",
     keywords="api wrapper datagouv",
-    license="http://www.fsf.org/licensing/licenses/agpl-3.0.html",
-    url="https://github.com/datagouv/datagouv_client",
+    license="https://spdx.org/licenses/MIT.html#licenseText",
+    url="https://www.data.gouv.fr",
+    project_urls={
+        "Documentation": "https://www.data.gouv.fr/fr/dataservices/api-catalogue-des-donnees-ouvertes-data-gouv-fr/",
+        "Source": "https://github.com/datagouv/datagouv_client",
+    },
     data_files=[
-        ("share/datagouv_client", ["CHANGELOG.md", "LICENSE.AGPL.txt", "README.md"]),
+        ("share/datagouv_client", ["CHANGELOG.md", "LICENSE", "README.md"]),
     ],
     entry_points={
         "console_scripts": [
             "datagouv_client=datagouv_client.cli:run",
         ],
     },
-    include_package_data=True,  # Will read MANIFEST.in
+    setup_requires=pip("requirements-build.txt"),
     install_requires=pip("requirements.txt"),
+    extras_require={
+        "dev": pip("requirements-dev.txt"),
+    },
     packages=find_packages(),
 )
