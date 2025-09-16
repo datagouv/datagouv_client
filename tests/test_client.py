df42cd97704c23f2784a3ab287d4c3db92028a5c
Alexandre Bulté <alexandre@bulte.net>
2025-09-16 10:50:24 +0200

    better test

--- tests/test_client.py
+++ tests/test_client.py
@@ -65,6 +65,7 @@ def test_get_all(args):
 
 def test_build_url():
     client = Client()
-    assert client._build_url("/api/2/coucou") == "https://www.data.gouv.fr/api/2/coucou"
-    assert client._build_url("https://www.data.gouv.fr/api/2/coucou") == "https://www.data.gouv.fr/api/2/coucou"
-    assert client._build_url("api/2/coucou") == "https://www.data.gouv.fr/api/2/coucou"
+    expected = "https://www.data.gouv.fr/api/2/coucou"
+    assert client._build_url("/api/2/coucou") == expected
+    assert client._build_url(expected) == expected
+    assert client._build_url("api/2/coucou") == expected
