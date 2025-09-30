2f484485cc2732f89d380542648ef7bde768cf0f
Pierlou <pierlou.ramade@data.gouv.fr>
2025-09-30 14:35:53 +0200

    chore: lint

--- datagouv/client.py
+++ datagouv/client.py
@@ -64,10 +64,14 @@ class Client:
             return result if isinstance(result, str) else None
 
         def cast_elem(elem: dict, client: Client, cast_as):
-            return elem if cast_as is None else cast_as(
-                elem["id"],
-                _client=client,
-                _from_response=elem,
+            return (
+                elem
+                if cast_as is None
+                else cast_as(
+                    elem["id"],
+                    _client=client,
+                    _from_response=elem,
+                )
             )
 
         headers = {}
