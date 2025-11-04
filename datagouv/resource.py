3a25136902944468d7641028f00a572fe462f651
Pierlou Ramade <48205215+Pierlou@users.noreply.github.com>
2025-11-04 12:06:55 +0100

    fix: community resources uri

--- datagouv/resource.py
+++ datagouv/resource.py
@@ -47,7 +47,7 @@ class Resource(BaseObject):
         self.uri = (
             f"{_client.base_url}/api/1/datasets/{self.dataset_id}/resources/{self.id}/"
             if not is_communautary and self.dataset_id is not None
-            else f"{_client.base_url}/api/1/datasets/community_resources/{self.id}"
+            else f"{_client.base_url}/api/1/datasets/community_resources/{self.id}/"
         )
         self.front_url = self.uri.replace("/api/1", "").replace("/resources", "/#/resources")
         if fetch or _from_response:
@@ -207,3 +207,4 @@ class ResourceCreator(Creator):
             payload.update({"type": "main"})
         r.update(payload=payload)
         return r
+
