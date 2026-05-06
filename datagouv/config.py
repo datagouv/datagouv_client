14fb02cfb9806dfbfc92504f2229b5f6f3ed08bd
Pierlou <pierlou.ramade@data.gouv.fr>
2026-05-06 15:53:08 +0200

    feat: add config logic

---
+++ datagouv/config.py
@@ -0,0 +1,15 @@
+import json
+from pathlib import Path
+
+CONFIG_PATH = Path.home() / ".datagouv_config.json"
+
+def save_config(environment: str, api_key: str):
+    CONFIG_PATH.write_text(json.dumps({"api_key": api_key, "environment": environment}))
+
+def load_config() -> dict:
+    if not CONFIG_PATH.exists():
+        raise RuntimeError("Please run `datagouv setup` to set up the config.")
+    return json.loads(CONFIG_PATH.read_text())
+
+def _delete_config():
+    CONFIG_PATH.unlink()
