29bd88d84ecf2f7dc4d9383e6abb01a8b382f924
Pierlou Ramade <48205215+Pierlou@users.noreply.github.com>
2026-05-19 15:28:34 +0200

    CLI: allow to remain anonymous when only getting data (#52)

--- datagouv/config.py
+++ datagouv/config.py
@@ -1,16 +1,24 @@
 import json
+import logging
 from pathlib import Path
 
 CONFIG_PATH = Path.home() / ".datagouv_config.json"
 
 
-def save_config(environment: str, api_key: str):
-    CONFIG_PATH.write_text(json.dumps({"api_key": api_key, "environment": environment}))
+def save_config(environment: str, api_key: str | None):
+    config = {"environment": environment}
+    if api_key:
+        config["api_key"] = api_key
+    CONFIG_PATH.write_text(json.dumps(config))
 
 
 def load_config() -> dict:
     if not CONFIG_PATH.exists():
-        raise RuntimeError("Please run `datagouv setup` to set up the config.")
+        logging.warning(
+            "No config has been specified, defaulting to prod environment. "
+            "Run `datagouv setup` to create a config file and get rid of this message."
+        )
+        return {}
     return json.loads(CONFIG_PATH.read_text())
 
 
